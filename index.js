cc6ef44d431afe08b53ff8f41dcfcf2fa75ad366
Mat Taylor <mtaylor@mtaylor-mbp.local>
2016-06-09 08:49:44 -0700

    init guard

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if (!this.fetch) var fetch = require('node-fetch') 
 
 function jsref(ob, opts)  {
-  if (!ob || typeof ob !== 'object') return ob
+  if (typeof ob !== 'object') return Promise.resolve(ob)
   if (!opts) opts = {}
   var root = opts.root || 'http://localhost/'
   var $ref = opts.$ref || '$ref'
