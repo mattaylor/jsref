a5619a692594d7fabd6fd89fbfb17c1e3e12f68f
Mat Taylor <mtaylor@mtaylor-mbp.local>
2016-06-10 09:55:15 -0700

    NO debug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fetch = fetch || require('node-fetch')
 
-function jsref(ob, opts) {
+function jsref(ob, opts={}) {
   if (typeof ob !== 'object') return ob
   opts = opts || {}
   var root = opts.root || 'http://localhost/'
@@ -9,7 +9,6 @@ function jsref(ob, opts) {
   var refs = {}
 
   var find = opts.find || function(url) {
-    console.log('find', url)
     url = url.indexOf('http') ? root+url : url
     var [url,ref] = url.split('#')
     ref = (ref && ref.length) ? ref : opts.frag
@@ -17,8 +16,7 @@ function jsref(ob, opts) {
     return opts.deep ? rec.then(rec => jsref(rec, opts)) : rec
   }
 
-  function extRefs(ref, val) {
-    console.log('extRefs', ref)
+  function extRefs(ref, val=ob) {
     if (opts.refs && opts.refs[ref]) return opts.refs[ref]
     val = val || ob
     if (ref[0] != '#') return opts.lazy ? find(ref) : vals.push(find(ref))
@@ -29,14 +27,12 @@ function jsref(ob, opts) {
   }
   
   function getRefs(ob) {
-    console.log('getRefs', ob)
     if (ob && ob[$ref]) {
       if (!refs[ob[$ref]]) refs[ob[$ref]] = extRefs(ob[$ref])
     } else for (var i in ob) if (typeof ob[i] === 'object') getRefs(ob[i])
   }
    
   function fixRefs(ob) {
-    console.log('fixRefs', ob)
     if (ob && ob[$ref]) return refs[ob[$ref]] || ob
     for (var k in ob) if (typeof ob[k] === 'object') ob[k] = fixRefs(ob[k])
     return ob
@@ -47,7 +43,6 @@ function jsref(ob, opts) {
   if (opts.lazy) return Promise.resolve(fixRefs(ob))
 
   return Promise.all(vals).then(recs => {
-    console.log('promAll', recs)
     for (var r in refs) if (!isNaN(refs[r])) refs[r] = recs[refs[r]-1]
     return fixRefs(ob)
   })
