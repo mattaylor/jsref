48b7dc2f3aaf0001bb445c3edc156163084f435d
Mat Taylor <mtaylor@mtaylor-mbp.local>
2016-06-10 17:14:22 -0700

    refactor

--- index.js
+++ index.js
@@ -8,38 +8,38 @@ function jsref(ob, opts={}) {
   var find = url => fetch(url.indexOf('http') ? root+url : url, opts.http).then(res => res.json())
   var vals = []
   
-  function extRefs(url) {
+  function extRef(url) {
     var [url,ref] = url.split('#')
     ref = (ref && ref.length) ? ref : opts.frag
-    var rec = (opts.find||find)(url).then(rec => ref ? setRefs('#'+ref,rec) : rec)
-    return opts.deep ? rec.then(getRefs) : rec
+    var rec = (opts.find||find)(url).then(rec => ref ? setRef('#'+ref,rec) : rec)
+    return opts.deep ? rec.then(getRef) : rec
   }
 
-  function setRefs(ref, val=ob) {
-    if (ref[0] != '#') return opts.lazy ? extRefs(ref) : vals.push(extRefs(ref))
+  function setRef(ref, val=ob) {
+    if (ref[0] != '#') return opts.lazy ? extRef(ref) : vals.push(extRef(ref))
     var keys = ref.substr(1).split(/[\.\/]/)
     if (!keys[0].length) keys.shift()
     while(val && keys.length) val = val[keys.shift()]
     return val
   }
 
-  function getRefs(ob) {
+  function getRef(ob) {
     if (ob && ob[$ref] && (!opts.like || RegExp(opts.like).test(ob[$ref]))) {
-      if (!refs[ob[$ref]]) refs[ob[$ref]] = setRefs(ob[$ref])
-    } else for (var i in ob) if (typeof ob[i] === 'object') getRefs(ob[i])
+      if (!refs[ob[$ref]]) refs[ob[$ref]] = setRef(ob[$ref])
+    } else for (var i in ob) if (typeof ob[i] === 'object') getRef(ob[i])
     return ob
   }
 
-  function fixRefs(ob) {
+  function fixRef(ob) {
     if (ob && ob[$ref]) return fixRefs(refs[ob[$ref]]) || ob
-    for (var k in ob) if (typeof ob[k] === 'object') ob[k] = fixRefs(ob[k])
+    for (var k in ob) if (typeof ob[k] === 'object') ob[k] = fixRef(ob[k])
     return ob
   }
 
-  getRefs(ob)
-  return opts.lazy ? Promise.resolve(fixRefs(ob)) : Promise.all(vals).then(recs => {
+  getRef(ob)
+  return opts.lazy ? Promise.resolve(fixRef(ob)) : Promise.all(vals).then(recs => {
     for (var r in refs) if (!isNaN(refs[r])) refs[r] = recs[refs[r]-1]
-    return fixRefs(ob)
+    return fixRef(ob)
   })
 }
 
